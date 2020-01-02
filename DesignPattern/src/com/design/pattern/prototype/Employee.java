a02004963d1e5d8ffcbf904c29760970b79c4c4b
jain-utkarsh <utkarsh.jain@hotmail.com>
2020-01-01 22:27:46 -0800

    Added implementation for the prototype design pattern

---
+++ DesignPattern/src/com/design/pattern/prototype/Employee.java
@@ -0,0 +1,55 @@
+package com.design.pattern.prototype;
+
+public class Employee implements Cloneable {
+
+	private String fName;
+	private String lName;
+	private String email;
+	
+	
+	public Employee(String fName, String lName, String email) {
+		super();
+		this.fName = fName;
+		this.lName = lName;
+		this.email = email;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [fName=" + fName + ", lName=" + lName + ", email=" + email + "]";
+	}
+
+	protected Object clone() {
+		Object clone = null;
+		try {
+			clone = super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return clone;
+	}
+}
